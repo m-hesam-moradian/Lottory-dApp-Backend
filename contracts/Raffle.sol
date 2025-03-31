ccb8c50e7ce3870d0bea5b7f1f5b0dd4bf40e17e
m.hesam moradian <m.hesam.moradian@gmail.com>
2025-03-31 21:14:35 +0330

    edit new funcs in Raffle.sol

--- contracts/Raffle.sol
+++ contracts/Raffle.sol
@@ -1,60 +1,61 @@
 // SPDX-License-Identifier: UNLICENSED
 pragma solidity ^0.8.28;
 
-// Uncomment this line to use console.log
-// import "hardhat/console.sol";
-
 contract Raffle {
-
     error Raffle__NotEnoughETHEntered();
     error Raffle__TransferFailed();
     error Raffle__NotOwner();
     error Raffle__NoPlayersEntered();
-    uint private immutable i_entraceAmount;
+    error Raffle__IndexOutOfBounds();
     
+    uint256 private immutable i_entranceAmount; 
     address payable[] private players;
-    address payable private owner;
+    address private immutable owner; 
 
-    // event Withdrawal(uint amount, uint when);
-
-    constructor(uint entraceAmount) payable {
-        i_entraceAmount = entraceAmount;
-        owner = payable(msg.sender);
+    constructor(uint256 entranceAmount) payable {
+        i_entranceAmount = entranceAmount;
+        owner = msg.sender; 
     }
 
-    function enterLottory() public payable {
-        if (msg.value < i_entraceAmount) {
+    function enterLottery() external payable { 
+        if (msg.value < i_entranceAmount) {
             revert Raffle__NotEnoughETHEntered();
         }
         players.push(payable(msg.sender));
     }
 
-    function getOwner() public view returns (address) {
+    function withdraw() external {
+        if (msg.sender != owner) revert Raffle__NotOwner();
+        
+        uint256 amount = address(this).balance;
+        if (amount == 0) revert Raffle__NoPlayersEntered();
+        
+       
+        delete players; 
+        
+        (bool success, ) = owner.call{value: amount}("");
+        if (!success) revert Raffle__TransferFailed();
+    }
+
+    // Getter functions optimized
+    function getOwner() external view returns (address) {
         return owner;
     }
-    function getBalance() public view returns (uint) {
+
+    function getBalance() external view returns (uint256) {
         return address(this).balance;
     }
-    function withdraw() public {
-        require(msg.sender == owner, "Only the owner can withdraw");
-        uint amount = address(this).balance;
-        (bool success, ) = owner.call{value: amount}("");
-        if (!success) {
-            revert Raffle__TransferFailed();
-        }
-   
-        players = new address payable[](0);
-        
+
+    function getEntranceAmount() external view returns (uint256) {
+        return i_entranceAmount;
     }
-    function getEntranceAmount() public view returns (uint) {
-        return i_entraceAmount;
+
+    function getPlayerCount() external view returns (uint256) {
+        return players.length;
     }
-    
-    function getAllPlayers() public view returns (address payable[] memory){
-        return players;
-    }
-    function getPlayersAtIndex(uint index) public view returns (address payable) {
-        require(index < players.length, "Index out of bounds");
+
+    function getPlayerAtIndex(uint256 index) external view returns (address) {
+        if (index >= players.length) revert Raffle__IndexOutOfBounds();
         return players[index];
     }
 }
--- hardhat.config.js
+++ hardhat.config.js
@@ -2,4 +2,5 @@
 /** @type import('hardhat/config').HardhatUserConfig */
 module.exports = {
   solidity: "0.8.28",
+  
 };
